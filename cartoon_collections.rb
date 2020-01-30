fd9a6937d9dd13084128e2d0de9ec0e95abd81f8
james Johnson <2013matthew2013@gmail.com>
2020-01-30 02:45:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,10 @@ planeteer_calls.map { |call| call.capitalize + '!' }
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
 
-def long_planeteer_calls# 
+def long_planeteer_calls# planeteer_calls.any? { |call| call.length > 4 }
+end
+
+long_planeteer_calls(planeteer_calls)
   short_words = ["puff", "go", "two"]
 long_planeteer_calls(short_words)
 end
