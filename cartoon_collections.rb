95db36b265e3d95a9309588c427ff03d4e9c6561
james Johnson <2013matthew2013@gmail.com>
2020-01-30 02:41:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves# (dwarves)
   ["Doc", "Dopey", "Bashful", "Grumpy"]
 end
 
-def summon_captain_planet# 
+def summon_captain_planet# (planeteer_calls)
 planeteer_calls.map { |call| call.capitalize + '!' }
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
